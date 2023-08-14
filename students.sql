9af8688342a47a39d8e9dd7bb861ba346180abad
06Nidhi1990 <142203141+06Nidhi1990@users.noreply.github.com>
2023-08-14 12:48:52 +0530

    Update students.sql

--- students.sql
+++ students.sql
@@ -34,27 +34,9 @@ CREATE TABLE `students` (
   `phone` varchar(191) NOT NULL,
   `course` varchar(191) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `students`
 --
 
-INSERT INTO `students` (`id`, `name`, `email`, `phone`, `course`) VALUES
-(2, 'Siri Prabhakar Shetty', 'shettysiriphysios@gmail.com', '9892210752', 'MPT(Cardio Pulmonary Physiotherapist)'),
-(3, 'Asha Shetty', 'aaashashetty@gmail.com', '9757057910', 'BAF'),
-(4, 'Mansi Sharma', 'mansisharma2002@gmail.com', '9076496121', 'BA'),
-(5, 'Shashikala Shetty', 'herganidhi06@gmail.com', '7738002448', 'MCA'),
-(7, 'Manisha Vishwakarma', 'mani1995@gmail.com', '9969885376', 'MSc Computer Science'),
-(8, 'Kashyap ', 'kashyap17@gmail.com', '9594253788', 'MSc IT'),
-(9, 'Rashmi Singh', 'rashmisingh_1995@gmail.com', '9969885376', 'MSc Computer Science'),
-(10, 'Prabhakar Shetty', 'hergaprabhakarshetty@gmail.com', '9969885376', 'BE(Chemical Engineering)'),
-(11, 'Nidhi Prabhakar Shetty', 'herganidhi06@gmail.com', '9757057904', 'MSc Computer Science'),
-(12, 'Lavanya Shetty', 'lavanyashetty39@gmail.com', '9164169855', 'BBM(Accounts & Finance)'),
-(13, 'Rajvi Bobale', 'rajvibobale404@gmail.com', '9221479512', 'BSc(Computer Science)'),
-(15, 'Rashi Shetty', 'rashishetty20@gmail.com', '8850906872', 'CA(Chartered Accountant)'),
-(16, 'Nishita Shetty', 'nishitashetty31@gmail.com', '9969762926', 'BE(Electronics & Telecommunication)'),
-(17, 'Arundhathi  Shetty', 'hergaprabhakarshetty@gmail.com', '9969885376', 'BE(Electronics & Telecommunication)'),
-(18, 'Jyoti Shetty', 'hergaprabhakarshetty@gmail.com', '7411377853', 'BA');
+
 
 --
 -- Indexes for dumped tables
